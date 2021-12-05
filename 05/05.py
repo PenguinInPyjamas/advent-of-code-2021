d002e4247656e44a950e669f1242ffd92aaeb8d4
Matthew Taylor <github@matthewtaylor.dev>
2021-12-05 13:20:20 +0000

    Day 05 cleanup

--- 05/05.py
+++ 05/05.py
@@ -1,6 +1,7 @@
 import argparse
 from collections import namedtuple
 from collections import defaultdict
+from itertools import repeat
 
 
 Point = namedtuple("Point", ['x', 'y'])
@@ -34,33 +35,25 @@ def main():
 def count_overlaps(vents, count_diagonals=False):
     overlap_counts = defaultdict(lambda: 0)
     for v in vents:
+        if not count_diagonals and v.start.x != v.end.x and v.start.y != v.end.y:
+            continue
+
         if v.start.x == v.end.x:
-            if v.start.y < v.end.y:
-                ys = range(v.start.y, v.end.y + 1)
-            else:
-                ys = range(v.end.y, v.start.y + 1)
-            for y in ys:
-                overlap_counts.update([(Point(v.start.x, y), overlap_counts[Point(v.start.x, y)] + 1)])
-        elif v.start.y == v.end.y:
-            if v.start.x < v.end.x:
-                xs = range(v.start.x, v.end.x + 1)
-            else:
-                xs = range(v.end.x, v.start.x + 1)
-            for x in xs:
-                overlap_counts[Point(x, v.start.y)] += 1
-        elif count_diagonals:
-            if v.start.x < v.end.x:
-                xs = range(v.start.x, v.end.x + 1)
-            else:
-                xs = range(v.start.x, v.end.x - 1, -1)
+            xs = repeat(v.start.x)
+        elif v.start.x < v.end.x:
+            xs = range(v.start.x, v.end.x + 1)
+        else:
+            xs = range(v.start.x, v.end.x - 1, -1)
 
-            if v.start.y < v.end.y:
-                ys = range(v.start.y, v.end.y + 1)
-            else:
-                ys = range(v.start.y, v.end.y - 1, -1)
+        if v.start.y == v.end.y:
+            ys = repeat(v.start.y)
+        elif v.start.y < v.end.y:
+            ys = range(v.start.y, v.end.y + 1)
+        else:
+            ys = range(v.start.y, v.end.y - 1, -1)
 
-            for x, y in zip(xs, ys):
-                overlap_counts[Point(x, y)] += 1
+        for x, y in zip(xs, ys):
+            overlap_counts[Point(x, y)] += 1
     return sum(1 if x > 1 else 0 for x in overlap_counts.values())
 
 
