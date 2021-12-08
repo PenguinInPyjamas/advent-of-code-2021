57c3254443b0ea637649241ef2683b8dd9cc8dde
Matthew Taylor <github@matthewtaylor.dev>
2021-12-08 19:46:59 +0000

    Day 08 Part 2

--- 08/08.py
+++ 08/08.py
@@ -13,49 +13,100 @@ def parse_args():
 
 
 def parse_input_line(l):
-    input_signals, output_signals = ([x.strip() for x in s.split(" ")] for s in l.split(" | "))
+    input_signals, output_signals = ([set(x.strip()) for x in s.split(" ")] for s in l.split(" | "))
     return SignalEntry(input_signals, output_signals)
 
 
 def main():
     signal_entries = parse_args()
-    output_digits = [get_correct_output_signal(s) for s in signal_entries]
+    output_digits = [get_output_digits(s) for s in signal_entries]
     part_1_answer = count_in_output_digits(output_digits, [1, 4, 7, 8])
-    part_2_answer = None
-    # part_2_answer = sum_output_digits(output_digits)
+    part_2_answer = sum_all_output_digits(output_digits)
     print(f"Part 1:\n{part_1_answer}\n\nPart 2:\n{part_2_answer}")
 
 
-def get_correct_output_signal(signal_entry):
-    unknown_output_signals = set(signal_entry.out_signals)
-    known_output_digits = []
-    potential_connections = dict((c, "abcdefg") for c in "abcdefg")
-    for s in signal_entry.out_signals:
+def get_output_digits(signal_entry):
+    known_signals = dict()
+    all_signals = signal_entry.in_signals + signal_entry.out_signals
+    for s in all_signals:
         if len(s) == 2:
-            if s in unknown_output_signals:
-                unknown_output_signals.remove(s)
-            known_output_digits.append(1)
+            known_signals[1] = s
         elif len(s) == 3:
-            if s in unknown_output_signals:
-                unknown_output_signals.remove(s)
-            known_output_digits.append(7)
+            known_signals[7] = s
         elif len(s) == 4:
-            if s in unknown_output_signals:
-                unknown_output_signals.remove(s)
-            known_output_digits.append(4)
+            known_signals[4] = s
         elif len(s) == 7:
-            if s in unknown_output_signals:
-                unknown_output_signals.remove(s)
-            known_output_digits.append(8)
-    return known_output_digits
+            known_signals[8] = s
+    for s in all_signals:
+        if len(s) == 5:
+            if known_signals[4] and count_signals_in_common(s, known_signals[4]) == 2:
+                known_signals[2] = s
+                break
+    for s in all_signals:
+        if len(s) == 5:
+            if known_signals[1] and count_signals_in_common(s, known_signals[1]) == 2:
+                known_signals[3] = s
+                break
+            elif known_signals[7] and count_signals_in_common(s, known_signals[7]) == 3:
+                known_signals[3] = s
+                break
+            elif known_signals[2] and count_signals_in_common(s, known_signals[2]) == 4:
+                known_signals[3] = s
+                break
+    for s in all_signals:
+        if len(s) == 5:
+            if known_signals[2] and known_signals[3] and count_signals_in_common(s, known_signals[2]) != 5 and count_signals_in_common(s, known_signals[3]) != 5:
+                known_signals[5] = s
+                break
+    for s in all_signals:
+        if len(s) == 6:
+            if known_signals[4] and count_signals_in_common(s, known_signals[4]) != 4\
+                    and known_signals[7] and count_signals_in_common(s, known_signals[7]) == 3:
+                known_signals[0] = s
+                break
+    for s in all_signals:
+        if len(s) == 6:
+            if known_signals[1] and count_signals_in_common(s, known_signals[1]) != 2:
+                known_signals[6] = s
+                break
+            elif known_signals[7] and count_signals_in_common(s, known_signals[7]) != 3:
+                known_signals[6] = s
+                break
+    for s in all_signals:
+        if len(s) == 6:
+            if known_signals[4] and count_signals_in_common(s, known_signals[4]) == 4:
+                known_signals[9] = s
+                break
+    return [signal_to_digit(x, known_signals) for x in signal_entry.out_signals]
+
+
+def count_signals_in_common(lhs, rhs):
+    return len(lhs.intersection(rhs))
+
+
+def signal_to_digit(signal, known_signals):
+    for known_digit, known_signal in known_signals.items():
+        if len(signal) == len(known_signal) and all(x in known_signal for x in signal):
+            return known_digit
+    raise Exception(f"signal_to_digit: Could not find digit for signal '{signal}'")
+
+
+def reduce_possible_connections(potential_connections, valid_in_segments, valid_out_segments):
+    for s in valid_in_segments:
+        potential_connections[s] = potential_connections[s].intersection(valid_out_segments)
 
 
 def count_in_output_digits(output_digits, target_digits):
     return sum(1 if x in target_digits else 0 for s in output_digits for x in s)
 
 
-def sum_output_digits(output_digits):
-    return sum(x for l in output_digits for x in l)
+def sum_all_output_digits(output_digits):
+    count = 0
+    for x in output_digits:
+        if len(x) != 4:
+            raise Exception(f"sum_all_output_digits: There should be exactly 4 digits, found digits of {x}")
+        count += x[0] * 1000 + x[1] * 100 + x[2] * 10 + x[3]
+    return count
 
 
 if __name__ == "__main__":
